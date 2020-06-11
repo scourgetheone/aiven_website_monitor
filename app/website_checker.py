c392615a922c0f86b51595cd6a1b69aa165fccfa
Viet Nguyen <viet.nguyen@csit.fi>
2020-06-11 15:46:44 +1000

    optimize and refactor code

--- app/website_checker.py
+++ app/website_checker.py
@@ -4,49 +4,99 @@ from timeloop import Timeloop
 from kafka import KafkaProducer
 from datetime import datetime, timedelta
 
+import re
 import requests
 import json
+
+from app.utils import AttrDict
 
 import app.utils as utils
 
 timeloop = Timeloop()
 
-producer = KafkaProducer(
-    value_serializer=lambda m: json.dumps(m).encode('ascii'),
-    **utils.read_json_config('kafka_config.json'),
-)
+def poll_website(producer, website, kafka_topic):
+    """Polls a website using the requests library
 
-def schedule_job(website):
+        Args:
+        - producer KafkaProducer: a KafkaProducer object
+        - website dict: the website dict object. Usually from utils.get_websites()
+        - kafka_topic str: the kafka topic to send messages to
+    """
+    response = None
 
-    @timeloop.job(interval=timedelta(seconds=website['check_interval']))
-    def poll_website():
+    try:
         response = requests.get(website['url'])
+    except requests.exceptions.InvalidSchema:
+        print ('website with id: {} has an invalid connection schema! Url: {}'.format(website['id'], website['url']))
+        return
+    except requests.exceptions.InvalidURL:
+        print ('website with id: {} has an invalid url! Url: {}'.format(website['id'], website['url']))
+        return
+    except requests.exceptions.RequestException as e:
+        print ('website with id: {} cannot be reached! Url: {}'.format(website['id'], website['url']))
+        response = AttrDict(
+            status_code='RequestException: {}'.format(e),
+            elapsed=AttrDict(
+                seconds=0,
+                microseconds=0,
+            ),
+            text=''
+        )
+    finally:
+        if not response:
+            return
+
         message = {
             'website_db_id': website['id'],
             'url': website['url'],
             'status_code': response.status_code,
             'elapsed': '{}.{}'.format(response.elapsed.seconds, response.elapsed.microseconds),
-            'timestamp_utc': str(datetime.now()),
+            'timestamp_utc': str(datetime.utcnow()),
         }
 
-        producer.send('website_status', message)
+        # Compare the regex pattern if set:
+        if website['up_regex']:
+            if re.search(website['up_regex'], response.text):
+                # Add additional information to the message
+                message['regex_match'] = True
+            else: message['regex_match'] = False
+
+        producer.send(kafka_topic, message)
         producer.flush()
 
+        return message
 
-def setup_polling():
-    """Sets up polling jobs after getting the website list from the DB
+def schedule_job(producer, website, kafka_topic):
+    """Schedules jobs using timeloop
+
+        Args:
+        - producer KafkaProducer: a KafkaProducer object
+        - website dict: the website dict object. Usually from utils.get_websites()
+        - kafka_topic str: the kafka topic to send messages to
     """
-    for website in utils.get_websites():
-        schedule_job(website)
+    @timeloop.job(interval=timedelta(seconds=website['check_interval']))
+    def timeloop_job():
+        poll_website(producer, website, kafka_topic)
 
-def run_polling():
+def run_checker(kafka_topic='website_status'):
     """Runs website polling jobs using timeloop
-    """
-    setup_polling()
-    timeloop.start(block=True)
 
-if __name__ == '__main__':
+        Args:
+        - kafka_topic str: the kafka topic to send messages to
+    """
     try:
-        run_polling()
+        producer = KafkaProducer(
+            value_serializer=lambda m: json.dumps(m).encode('ascii'),
+            **utils.read_json_config('kafka_config.json'),
+        )
+
+        for website in utils.get_websites():
+            schedule_job(producer, website, kafka_topic)
+
+        timeloop.start(block=True)
+
     except KeyboardInterrupt:
         producer.close()
+
+if __name__ == '__main__':
+    run_checker()
