b2affd3b4fac447312ddc00090b760c53ee0c431
Viet Nguyen <viet.nguyen@csit.fi>
2020-06-11 17:00:26 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiven-website-monitor", # Replace with your own username
-    version="0.0.1",
+    version="0.1.0",
     author="Viet Nguyen",
     author_email="vietnguyen92@hotmail.com",
     description="An app that monitors website online status using Aiven's Kafka and PostgreSQL",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/scourgetheone",
+    url="https://github.com/scourgetheone/aiven_website_monitor",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Linux",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.4',
 )
