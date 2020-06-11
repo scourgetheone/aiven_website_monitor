95c9555240dbd8f3808c6cbcfaac34bf2d750812
Viet Nguyen <viet.nguyen@csit.fi>
2020-06-11 15:40:40 +1000

    handle various exceptions + some refactoring

--- app/broker_db_sync.py
+++ app/broker_db_sync.py
@@ -1,31 +1,48 @@
 # https://help.aiven.io/en/articles/489573-getting-started-with-aiven-postgresql
 # http://maximilianchrist.com/python/databases/2016/08/13/connect-to-apache-kafka-from-python-using-ssl.html
 
-from kafka import KafkaConsumer
+from json.decoder import JSONDecodeError
+from psycopg2.errors import (
+    UniqueViolation,
+    ForeignKeyViolation,
+    InvalidDatetimeFormat,
+)
 
+import kafka
 import json
 
 import app.utils as utils
 
-def sync_to_db():
+def sync_to_db(kafka_topic='website_status', test=False):
+    """Fetches messages from Kafka and persist them to the pgsql database
+
+        Args:
+        - kafka_topic str: the name of the kafka topic
+        - test bool: if True, sets the offset to 'earliest'
+          and return the first result only
+    """
     try:
-        consumer = KafkaConsumer(
-            'website_status',
-            #auto_offset_reset='earliest',
-            #enable_auto_commit=False,
+        # HACK: auto_offset_reset is set to earliest when testing
+        # because I can't figure out (yet) why the in test_end_to_end_function
+        # the broker always starts from offset 1 (should start from 0?)
+        consumer = kafka.KafkaConsumer(
+            kafka_topic,
+            auto_offset_reset='earliest' if test else 'latest',
             group_id='test_group',
             **utils.read_json_config('kafka_config.json'),
         )
 
-        # Read from website_status topic and sync to the database
-        for message in consumer:
-            print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                message.offset, message.key,
-                message.value))
+        with utils.connect_to_db(test=test, include_conn=True) as conn_obj:
+            conn, cursor = conn_obj
 
-            json_message = json.loads(message.value)
+            # Read from website_status topic and sync to the database
+            for message in consumer:
+                print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                    message.offset, message.key,
+                    message.value))
 
-            with utils.connect_to_db() as cursor:
+                json_message = json.loads(message.value)
+
                 cursor.execute('INSERT INTO website_status\
                     (kafka_topic, kafka_partition_id, kafka_offset_id, website_id, timestamp_utc, request_info)\
                     VALUES (%s, %s, %s, %s, %s, %s)',
@@ -38,6 +55,25 @@ def sync_to_db():
                         json.dumps(json_message),
                     )
                 )
+                # Commit after every successful fetch from the Kafka broker
+                conn.commit()
+
+                if test:
+                    consumer.close()
+                    return message
+
+    except JSONDecodeError as e:
+        print(e)
+        return message, JSONDecodeError
+    except UniqueViolation as e:
+        print(e)
+        return message, UniqueViolation
+    except ForeignKeyViolation as e:
+        print(e)
+        return message, ForeignKeyViolation
+    except InvalidDatetimeFormat as e:
+        print(e)
+        return message, InvalidDatetimeFormat
 
     except KeyboardInterrupt:
         consumer.close()
